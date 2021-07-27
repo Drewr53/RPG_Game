d57124d4286723f093543daa15e6f57cb69e556d
Drew Reddig <drewr53@gmail.com>
2021-07-26 22:07:49 -0400

    need to add check for mp left

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ while running:
         magic_dmg = player.generate_spell_damage(magic_index)
         player.reduce_mp(int(player.get_spell_mp_cost(magic_index)))
         enemy.take_damage(magic_dmg)
-        print("You attacked for", magic_dmg, "points of damage. You have ", player.get_mp, "Enemy HP:", enemy.get_hp())
+        print("You attacked for", magic_dmg, "points of damage. You have", player.get_mp(),"mp left. Enemy HP:", enemy.get_hp())
